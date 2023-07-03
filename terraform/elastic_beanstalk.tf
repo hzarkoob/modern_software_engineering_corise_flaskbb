4830635bf3842107c892a1d244ed876754d82b52
Hadi Zarkoob <hadi.zarkoob114@gmail.com>
2023-07-03 05:41:48 +0000

    terraform workflow test 2

--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -2,7 +2,7 @@ resource "aws_elastic_beanstalk_application" "application" {
     name = "flaskbb"  
 }
 # CoRise TODO: change cname_prefix to reflect your unique application name
-# test PR
+# test PR - 2
 resource "aws_elastic_beanstalk_environment" "environment" {
   name                = "flaskbb-environment"
   cname_prefix        = "hzarkoobflaskbb"

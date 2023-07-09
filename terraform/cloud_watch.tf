2049a062e3558103d6bd00e5671eb72b0af4c853
Hadi Zarkoob <hadi.zarkoob114@gmail.com>
2023-07-09 03:16:56 +0000

    watchtower2

--- terraform/cloud_watch.tf
+++ terraform/cloud_watch.tf
@@ -1,21 +1,24 @@
-# CoRise TODO: create a new log group
-
+# # CoRise TODO: create a new log group
+# resource "aws_cloudwatch_log_group" "flaskbb_app" {
+#   name              = "flaskbb_application"
+#   retention_in_days = 3
+# }
 # CoRise TODO: create a new dashboard
-resource "aws_cloudwatch_dashboard" "flaskbb_main" {
-  dashboard_name = "flaskbb_main"
+# resource "aws_cloudwatch_dashboard" "flaskbb_main" {
+#   dashboard_name = "flaskbb_main"
 
-  dashboard_body = jsonencode({})
-}
+#   dashboard_body = jsonencode({})
+# }
 
-#CoRise TODO: create metric alters
-resource "aws_cloudwatch_metric_alarm" "bot_attack" {
-  alarm_name                = "bot_attack"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = 2
-}
+# #CoRise TODO: create metric alters
+# resource "aws_cloudwatch_metric_alarm" "bot_attack" {
+#   alarm_name          = "bot_attack"
+#   comparison_operator = "GreaterThanOrEqualToThreshold"
+#   evaluation_periods  = 2
+# }
 
-resource "aws_cloudwatch_metric_alarm" "too_busy" {
-  alarm_name                = "too_busy"
-  comparison_operator       = "LessThanOrEqualToThreshold"
-  evaluation_periods        = 2
-}
+# resource "aws_cloudwatch_metric_alarm" "too_busy" {
+#   alarm_name          = "too_busy"
+#   comparison_operator = "LessThanOrEqualToThreshold"
+#   evaluation_periods  = 2
+# }

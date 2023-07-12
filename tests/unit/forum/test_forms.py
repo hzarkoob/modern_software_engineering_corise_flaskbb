2a3926b75c92919bf93edd06b6a98e5167eb5788
Hadi Zarkoob <hzarkoob@gmail.com>
2023-07-12 12:56:49 -0700

    wsl ide local test

--- tests/unit/forum/test_forms.py
+++ tests/unit/forum/test_forms.py
@@ -2,7 +2,7 @@ import pytest
 from flaskbb.forum import forms
 from werkzeug.datastructures import MultiDict
 
-# testttt445!!!
+# wsl ide local test
 
 pytestmark = pytest.mark.usefixtures("post_request_context", "default_settings")
 

897ab9e44e7227e753eb76e5313c94f80d5faff2
Hadi Zarkoob <hzarkoob@gmail.com>
2023-07-12 13:15:20 -0700

    wsl test w/o cred manager

--- tests/unit/forum/test_forms.py
+++ tests/unit/forum/test_forms.py
@@ -1,7 +1,7 @@
 import pytest
 from flaskbb.forum import forms
 from werkzeug.datastructures import MultiDict
-
+# wsl test
 # wsl ide local test2
 
 pytestmark = pytest.mark.usefixtures("post_request_context", "default_settings")

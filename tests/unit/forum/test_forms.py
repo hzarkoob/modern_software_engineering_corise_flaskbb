cf3769a81b4922a00c75c5c2396422464078c0bb
Hadi Zarkoob <hzarkoob@gmail.com>
2023-07-12 08:29:23 -0700

    test wsl 2

--- tests/unit/forum/test_forms.py
+++ tests/unit/forum/test_forms.py
@@ -2,7 +2,7 @@ import pytest
 from flaskbb.forum import forms
 from werkzeug.datastructures import MultiDict
 
-# testttt444!!!
+# testttt445!!!
 
 pytestmark = pytest.mark.usefixtures("post_request_context", "default_settings")
 

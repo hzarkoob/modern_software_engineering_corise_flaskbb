3861964003c7f607226ec1819c54818d627838fc
Hadi Zarkoob <hadi.zarkoob114@gmail.com>
2023-07-12 19:59:53 -0700

    integration test

--- tests/integration/test_home_page.py
+++ tests/integration/test_home_page.py
@@ -7,6 +7,11 @@
 # Hint: you can get the test client by calling `application.test_client()`
 # when using the application test fixture.
 
-# ADD CODE HERE
-
+def test_index_route(application, default_settings, default_groups, default_translations):
+    with application.test_client() as test_client:
+        response = test_client.get('/')
+        assert response.status_code == 200
+        assert b"Forum" in response.data
+        assert b"Portal" in response.data
+        
 ###################################################################
